e54f059ca62c8f33cc660fe7978d9e67c65271f2
Lance Conry <lance@rhinosw.com>
2026-06-03 15:12:27 +1000

    Updating server headers and redirect.

--- src/Sat/server/deploy.ps1
+++ src/Sat/server/deploy.ps1
@@ -6,6 +6,6 @@ $keyPath = (Convert-Path "~/.ssh/id_rsa")
 $knownHostsPath = (Convert-Path "~/.ssh/known_hosts")
 $target = "outerplanet@conryclan.com:~/conryclan.com/projects/satellite/sstv/"
 rsync -av -e "/usr/bin/ssh -i $keyPath -o UserKnownHostsFile=$knownHostsPath" `
-	--include="*/" --include="*.php" --exclude="*" `
+	--include="*/" --include="*.php" --include=".htaccess" --exclude="*" `
 	--chmod=D755,F644 `
 	./ $target
